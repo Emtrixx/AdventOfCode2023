a383bcedc8805d0041a1915c8fdc3ff8c3511d00
Jesse Guenzl <contact@jesseguenzl.com>
2023-12-01 21:58:24 +0200

    part two day 1

--- src/aoc/day01/mod.rs
+++ src/aoc/day01/mod.rs
@@ -1,10 +1,8 @@
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::BufRead;
+use std::io::{BufRead, BufReader, Lines};
 
 pub fn day01() {
-    println!("Hello, world!");
-
     let mut numbers: HashMap<&str, u32> = HashMap::new();
     numbers.insert("one", 1);
     numbers.insert("two", 2);
@@ -25,62 +23,70 @@ pub fn day01() {
     numbers.insert("8", 8);
     numbers.insert("9", 9);
 
+    let mut part_one_sum = 0;
     let mut sum = 0;
-    let mut first_num: u32;
-    let mut last_num: u32;
-    let mut smallest_index: usize;
-    let mut biggest_index: usize;
 
-    let file = File::open("src/aoc/day01/input.txt").unwrap();
-    let reader = std::io::BufReader::new(file);
-    let lines = reader.lines();
+    let lines = load_input();
+
     for line in lines {
         let line = line.unwrap();
 
-        /* First Half */
-        first_num = 0;
-        last_num = 0;
+        part_one_sum += part_one(&line);
 
-        // line.chars().for_each(|c| {
-        //     if (c.is_digit(10)) {
-        //         let num = c.to_digit(10).unwrap();
-        //         if first_num == 0 {
-        //             first_num = num;
-        //             last_num = num;
-        //         } else {
-        //             last_num = num;
-        //         }
-        //     }
-        // });
-
-        /* Second Half */
-
-        smallest_index = 1000;
-        biggest_index = 0;
-
-        for num in numbers.iter() {
-            if let Some(index) = line.find(num.0) {
-                // if first_num == 0 {
-                //     first_num = index as u32;
-                //     last_num = index as u32;
-                // } else {
-                //     last_num = index as u32;
-                // }
-                //
-                if index < smallest_index {
-                    smallest_index = index;
-                    first_num = *num.1;
-                }
-
-                if index >= biggest_index {
-                    biggest_index = index;
-                    last_num = *num.1;
-                }
-            }
-        }
-
-        sum += (first_num * 10) + last_num;
+        sum += part_two(&mut numbers, &line);
     }
 
-    println!("{}", sum);
+    println!("Part One: {}", part_one_sum);
+    println!("Part Two: {}", sum);
+}
+
+fn load_input() -> Lines<BufReader<File>> {
+    let file = File::open("src/aoc/day01/input.txt").unwrap();
+    let reader = std::io::BufReader::new(file);
+    let lines = reader.lines();
+    lines
+}
+
+fn part_two(numbers: &mut HashMap<&str, u32>, line: &String) -> u32 {
+    /* Second Half */
+    let mut first_num = 0;
+    let mut last_num = 0;
+    let mut smallest_index = 1000;
+    let mut biggest_index = 0;
+
+    for num in numbers.iter() {
+        if let Some(index) = line.find(num.0) {
+            if index < smallest_index {
+                smallest_index = index;
+                first_num = *num.1;
+            }
+        }
+        if let Some(index) = line.rfind(num.0) {
+            if index >= biggest_index {
+                biggest_index = index;
+                last_num = *num.1;
+            }
+        }
+    }
+
+    return (first_num * 10) + last_num;
+}
+
+fn part_one(line: &String) -> u32 {
+    /* First Half */
+    let mut first_num = 0;
+    let mut last_num = 0;
+
+    line.chars().for_each(|c| {
+        if (c.is_digit(10)) {
+            let num = c.to_digit(10).unwrap();
+            if first_num == 0 {
+                first_num = num;
+                last_num = num;
+            } else {
+                last_num = num;
+            }
+        }
+    });
+    return (first_num * 10) + last_num;
 }
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,4 +3,5 @@ use wgpu_template::aoc::day01::day01;
 fn main() {
     // println!("Hello, world!");
     day01();
+    // part_two();
 }
